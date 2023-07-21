087da81138cd8ff09404fe9cc1c58ca915710bad
JunRyeol <wnsfuf0121@naver.com>
2023-07-21 22:57:31 +0900

    [FastAPI] Seperate directory

---
+++ 0_first_project/app/controllers/item_controller.py
@@ -0,0 +1,21 @@
+from typing import Union
+from fastapi import APIRouter
+
+from ..models.item import Item
+
+router = APIRouter()
+
+@router.get()
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@router.post("/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax":price_with_tax})
+    return item_dict
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 참고: https://fastapi.tiangolo.com/ko/
 
 ### 명령어
-- 실행: `uvicorn main:app --reload`
+- 실행: `uvicorn app.main:app --reload`
     - main: main.py 파일
     - app: main.py 파일 내에 FastAPI()를 가지고 있는 객체
     - --reload: 코드가 변경된 후 서버 재시작 (개발 환경에서 사용)
